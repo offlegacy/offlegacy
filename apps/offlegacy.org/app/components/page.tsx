1c05f11526dd4aad9fa27382aa2ac92141f3b7d4
gwansikk <Seorit98@gmail.com>
2024-08-16 13:02:03 +0900

    feat: add page component

---
+++ apps/offlegacy.org/app/components/page.tsx
@@ -0,0 +1,11 @@
+import { type ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+const Page = ({ children }: Props) => {
+  return <main className="flex flex-col max-w-screen-xl p-4 m-auto space-y-10">{children}</main>
+}
+
+export default Page
--- apps/offlegacy.org/app/page.tsx
+++ apps/offlegacy.org/app/page.tsx
@@ -1,14 +1,10 @@
 import Image from 'next/image'
 import PackageCard from './components/package-card'
+import Page from './components/page'
 
 export default function Home() {
   return (
-    <main className="flex flex-col max-w-screen-xl p-4 m-auto space-y-10">
-      <section className="mx-auto flex flex-col items-center space-y-1.5">
-        <Image src="/offlegacy.svg" alt="offlegacy logo" width={256} height={256} priority />
-        <h1 className="text-3xl font-bold">OffLegacy</h1>
-        <h2 className="text-xl font-semibold">Open-source Development Team in South Korea 🇰🇷</h2>
-      </section>
+    <Page>
       <section className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <PackageCard>
           <PackageCard.Title>Palette Kit</PackageCard.Title>
@@ -17,6 +13,6 @@ export default function Home() {
           </PackageCard.Description>
         </PackageCard>
       </section>
-    </main>
+    </Page>
   )
 }
