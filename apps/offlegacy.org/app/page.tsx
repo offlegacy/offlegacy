6ced9b2bdb89a389bb07c11a11cbd6db7dd73c25
gwansikk <Seorit98@gmail.com>
2024-08-16 13:02:35 +0900

    feat: add Query Fetch and Vitest React Native package cards

--- apps/offlegacy.org/app/page.tsx
+++ apps/offlegacy.org/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import PackageCard from './components/package-card'
 import Page from './components/page'
 import AboutTeamCard from './components/about-team-card'
@@ -13,6 +12,14 @@ export default function Home() {
           <PackageCard.Description>
             🎨 Manage Design Tokens more easily and conveniently in a CSS-in-JS environment.
           </PackageCard.Description>
+        </PackageCard>
+        <PackageCard>
+          <PackageCard.Title>Query Fetch</PackageCard.Title>
+          <PackageCard.Description>🔗 Lightweight and Easy-to-Use Fetch API Extension Library.</PackageCard.Description>
+        </PackageCard>
+        <PackageCard>
+          <PackageCard.Title>Vitest React Native</PackageCard.Title>
+          <PackageCard.Description>⚡️⚛️📱 vitest plugin for react-native</PackageCard.Description>
         </PackageCard>
       </section>
     </Page>
